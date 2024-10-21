136db366592c6294d221ec331239fbfa62b11762
leila-P <leilapoormasti@gmail.com>
2024-10-21 11:28:41 -0700

    remove host and port

--- main.py
+++ main.py
@@ -14,5 +14,3 @@ async def init_tables():
 
 app.include_router(user_router, prefix="/users")
 
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 8000)))
